1ea8d53db55ad6a1f7802eb8d518114573b5310d
syl08 <syinlong08@gmail.com>
2022-07-08 14:17:30 +1000

    config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Forexco')
+    .setDescription('code challenge API')
+    .setVersion('1.0')
+    .addTag('forexco')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();

ad7b20755039af94570eb7d8041f1d61b3f80273
syl08 <syinlong08@gmail.com>
2022-07-08 14:29:08 +1000

    set global prefix with api, set validation pipe to avoid empty data, set user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateAuthDto {
+  @ApiProperty({
+    name: 'username',
+  })
+  @IsNotEmpty({ message: 'username is required' })
+  username: string;
+  @ApiProperty({
+    name: 'password',
+  })
+  @IsNotEmpty({ message: 'password is required' })
+  password: string;
+}

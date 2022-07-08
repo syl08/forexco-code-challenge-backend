cc93e24b47800e01c0d83247bae4c7a89599419c
syl08 <syinlong08@gmail.com>
2022-07-08 14:11:14 +1000

    generate rate CRUD resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
+import { RateModule } from './rate/rate.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, RateModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })

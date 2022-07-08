6cd7f729c1122ad572611268820cb81b50f590ab
syl08 <syinlong08@gmail.com>
2022-07-08 14:12:50 +1000

    generate auth CRUC resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
 import { RateModule } from './rate/rate.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, RateModule],
+  imports: [UserModule, RateModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })

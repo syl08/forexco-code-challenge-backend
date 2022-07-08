45b159780fbd5c289ce9e0730de5e723374f5d20
syl08 <syinlong08@gmail.com>
2022-07-08 23:28:12 +1000

    create register fcuntion in user.service and invoke it in user.controller

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
-export class CreateAuthDto {
+export class CreateUserDto {
   @ApiProperty({
     name: 'username',
   })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(private readonly prismaService: PrismaService) {}
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async register(createUserDto: CreateUserDto) {
+    // before insert user information, check username exists or not in database
+    const existedUser = await this.prismaService.user.findUnique({
+      where: { username: createUserDto.username },
+    });
+    if (existedUser) {
+      throw new ConflictException('username exists');
+    }
+    // hash password before insert to database
+    const hashPassword = bcrypt.hashSync(createUserDto.password, 10);
+    const newUser = this.prismaService.user.create({
+      data: {
+        username: createUserDto.username,
+        password: hashPassword,
+      },
+    });
+    return newUser;
   }
 }

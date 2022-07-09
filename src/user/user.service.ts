988e45ec5844cab2a244ba95d060368adfc24950
syl08 <syinlong08@gmail.com>
2022-07-09 16:29:06 +1000

    unit test for user.controller

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
